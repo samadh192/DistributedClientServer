4abfd5191b88fdbc5288ece36b24621ff96f00a7
samadh192 <azathabdulsamadh@gmail.com>
2023-09-29 23:53:41 -0500

    Added server and client logic

---
+++ Client/Client.fs
@@ -0,0 +1,26 @@
+﻿open System
+open System.Net
+open System.Net.Sockets
+open System.Text
+
+let connectToServer () =
+    let serverIpAddress = IPAddress.Parse("127.0.0.1") // Replace with the server's IP address
+    let serverPort = 12345 // Replace with the server's port number
+
+    let client = new TcpClient()
+    client.Connect(serverIpAddress, serverPort)
+
+    let stream = client.GetStream()
+
+    // Receive and print data from the server
+    let bufferLength = 256
+    let buffer = Array.zeroCreate<byte> bufferLength
+    let bytesRead = stream.Read(buffer, 0, buffer.Length)
+
+    // Convert the received bytes to a string
+    let message = Encoding.ASCII.GetString(buffer, 0, bytesRead)
+    Console.WriteLine("Received from server: {0}", message)
+
+    client.Close()
+
+connectToServer()
---
+++ Server/Server.fs
@@ -0,0 +1,33 @@
+﻿open System
+open System.Net
+open System.Net.Sockets
+open System.Text
+
+let startServer () =
+    let ipAddress = IPAddress.Parse("127.0.0.1") // Replace with your desired IP address
+    let port = 12345 // Replace with your desired port number
+
+    let listener = new TcpListener(ipAddress, port)
+    listener.Start()
+
+    Console.WriteLine("Server listening on {0}:{1}", ipAddress, port)
+
+    while true do
+        try
+            let client = listener.AcceptTcpClient()
+            Console.WriteLine("Client connected: {0}", client.Client.RemoteEndPoint)
+
+            // Send "Hello" to the client
+            let helloMessage = "Hello"
+            let helloBytes = Encoding.ASCII.GetBytes(helloMessage)
+            let stream = client.GetStream()
+            stream.Write(helloBytes, 0, helloBytes.Length)
+
+            // Handle client communication (e.g., reading/writing data)
+            // You can create a separate function or use async workflows here
+
+            client.Close()
+        with
+        | ex -> Console.WriteLine("Error: {0}", ex.Message)
+
+startServer()
