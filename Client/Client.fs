6bdc7b72c40a044b2bc8d5491058758dcfcb78ff
samadh192 <azathabdulsamadh@gmail.com>
2023-10-04 22:12:32 -0400

    Cosmetic changes to match expected output Added comments

--- Client/Client.fs
+++ Client/Client.fs
@@ -4,51 +4,95 @@ open System.Net.Sockets
 open System.Text
 open System.Threading
 
-let serverIpAddress = IPAddress.Parse("127.0.0.1") // Replace with the server's IP address
-let serverPort = 12345 // Replace with the server's port number
+// Server IP Address
+let serverIpAddress = IPAddress.Parse("127.0.0.1")
 
+// Server Port Number
+let serverPort = 12345
+
+
+// Function to connect to the server using IP address and port number
 let connectToServer () =
     let client = new TcpClient()
     client.Connect(serverIpAddress, serverPort)
     client
 
+// Function to send messages from client to server
 let sendToServer (client: TcpClient) (cancellationToken: CancellationToken) =
     async {
+        // Establish stream to communicate with server
         let stream = client.GetStream()
+        // Cancellation token is used to determine if sendToServer thread must continue
         while not cancellationToken.IsCancellationRequested do
-            // Console.Write("Enter a message to send to the server (or 'bye' to exit): ")
+            // Sleep for 500 milliseconds to ensure prompt is printed after server response
+            Async.Sleep(500) |> Async.RunSynchronously
+            Console.Write("\nSending command: ")
+
+            // Wait for user input
             let userInput = Console.ReadLine()
             let inputBytes = Encoding.ASCII.GetBytes(userInput)
+
+            // Forward user input to server
             stream.Write(inputBytes, 0, inputBytes.Length)
     }
 
+
+// Function to receive messages from server
 let receiveFromServer (client: TcpClient) (cancellationToken: CancellationTokenSource)=
     async {
+        // Establish stream to communicate with server
         let stream = client.GetStream()
+
+        // Create buffer to store received messages from server
         let bufferLength = 256
         let buffer = Array.zeroCreate<byte> bufferLength
+
+        // continueCommunication flag will be true as long as we dont encounter terminate
         let mutable continueCommunication = true
         while continueCommunication do
+            // Reach from server
             let bytesRead = stream.Read(buffer, 0, buffer.Length)
             let receivedMessage = Encoding.ASCII.GetString(buffer, 0, bytesRead)
-            Console.WriteLine("Received from server: {0}", receivedMessage)
+            let mutable serverResponse = ""
+
+            // Match received message to server response 
+            serverResponse <- match receivedMessage with
+                                | "-1" -> "incorrect operation command"
+                                | "-2" -> "number of inputs is less than two"
+                                | "-3" -> "number of inputs is more than four"
+                                | "-4" -> "one or more of the inputs contain(s) non-number(s)"
+                                | "-5" -> "exit"
+                                | "Hello!" -> "Hello!"
+                                | _     -> receivedMessage
+            // Display server response
+            Console.WriteLine("\nServer Response: {0}", serverResponse)
+
+            // If code -5 is received then the client must close
             if receivedMessage = (-5).ToString() then
                 continueCommunication <- false
+                // Indicate to sendToServer that it needs stop executing
                 cancellationToken.Cancel()
                 Environment.Exit(0)
     }
 
 [<EntryPoint>]
 let main argv =
+
+    // Create client object
     let client = connectToServer()
+
+    // Create cancellation token to allowe commincation between sendToServer
+    // and receiveFromServer threads
     let cts = new CancellationTokenSource()
-    
+
+    // Create asynchronous tasks to run sendToServer and receiveFromServer threads
     let sendTask = async { do! sendToServer client cts.Token }
     let receiveTask = async { do! receiveFromServer client cts}
 
     let tasks = [sendTask; receiveTask]
 
     try
+        // Parallely run sendToServer and receiveFromServer threads
         Async.RunSynchronously (Async.Parallel tasks) |> ignore
     with
         | :? OperationCanceledException ->
--- Server/Server.fs
+++ Server/Server.fs
@@ -5,12 +5,17 @@ open System.Collections.Generic
 open System.Text
 open System.Threading
 
-
+// Creating cancellation token to indicate server to exit
 let cancellationTokenSource = new CancellationTokenSource()
+
+// List to store connected clients
 let clients = new List<TcpClient>()
+
+// Map to store relationship between actual client endpoint and client ID
 let ClientMap = new Dictionary<EndPoint, int>()
 let nextClientNumber = ref 1
 
+// Function to return clientNumber from actual client endpoint
 let getClientNumber endPoint = 
     if ClientMap.ContainsKey(endPoint) then
         ClientMap.[endPoint]
@@ -20,26 +25,31 @@ let getClientNumber endPoint =
         nextClientNumber := clientNumber + 1
         clientNumber
 
+// Function to broadcast -5(exit code) to all clients after terminate
 let broadcastMessage (message: string) =
     let messageBytes = Encoding.ASCII.GetBytes(message)
     let disconnectedClients = ref []
 
     lock clients (fun () ->
+        // For each connected client send -5 to it
         for client in clients do
             try
                 let stream = client.GetStream()
                 stream.Write(messageBytes, 0, messageBytes.Length)
+                Console.WriteLine("Responding to client {0} with result: -5", getClientNumber client.Client.RemoteEndPoint)
             with
             | :? System.IO.IOException ->
                 disconnectedClients := client :: !disconnectedClients
             | :? System.ObjectDisposedException ->
-                Console.WriteLine("System.ObjectDisposedException caught")
+                ignore()
+
     )
 
     // Remove disconnected clients
     for disconnectedClient in !disconnectedClients do
         lock clients (fun () -> clients.Remove(disconnectedClient)) |> ignore
 
+// Function to perform addition
 let add result = 
     let mutable continueProcessing = true
     if Array.length result < 3 then 
@@ -59,7 +69,7 @@ let add result =
                     sum <- -4
         sum
     
-
+// Function to perform multiplication
 let mult result = 
     let mutable continueProcessing = true
     if Array.length result < 3 then 
@@ -79,6 +89,7 @@ let mult result =
                     product <- -4
         product
 
+// Function to perform subtraction
 let subtract result = 
     let mutable continueProcessing = true
     if Array.length result < 3 then 
@@ -101,36 +112,55 @@ let subtract result =
                     sub <- -4
         sub
 
+// Function to service client
 let handleClient (client : TcpClient) =
     async {
+        // Retrieve clientID
         let clientID = client.Client.RemoteEndPoint
-        Console.WriteLine("Client connected: {0}", getClientNumber clientID)
+
+        // Establish stream to communicate with client
+        let stream = client.GetStream()
+
+        // Store client in 
         lock clients (fun () -> clients.Add(client))
 
+        // Send Hello! to client when it connects
+        let responseBytes = Encoding.ASCII.GetBytes("Hello!")
+        do! Async.AwaitTask (stream.WriteAsync(responseBytes, 0, responseBytes.Length))
 
-        let stream = client.GetStream()
+        // Create buffer to store messages received from client
         let bufferLength = 256
         let buffer = Array.zeroCreate<byte> bufferLength
 
         let mutable continueCommunication = true
         while continueCommunication do
             try
+                // Read messages from client
                 let! bytesRead = Async.AwaitTask (stream.ReadAsync(buffer, 0, buffer.Length))
 
+                // If message is empty stop communication
                 if bytesRead = 0 then
                     continueCommunication <- false
                 else
+                    // Display received message from client
                     let receivedMessage = Encoding.ASCII.GetString(buffer, 0, bytesRead)
+                    Console.WriteLine("Received: {0}",receivedMessage)
                     let mutable res = 0
+
+                    // If received message is terminate then stop communication and send 
+                    // -5(exit code) to all connected clients so that they all exit
                     if receivedMessage.Trim().ToLower() = "terminate" then
                         res <- -5
                         continueCommunication <- false
                         broadcastMessage "-5"
+
+                        // Indicate server to exit
                         cancellationTokenSource.Cancel() 
-                        Environment.Exit(0) // Exit the application forcefully
-                    else 
+                        Environment.Exit(0)
+
+                    // For other messages process them according to the operation type
+                    else
                         let result = receivedMessage.Split ' '
-                        
                         res <-   match result[0] with
                                     | "add" -> add result
                                     | "multiply" -> mult result
@@ -138,6 +168,7 @@ let handleClient (client : TcpClient) =
                                     | "bye" -> -5
                                     | _ -> -1
 
+                    // Send response back to client
                     let responseMessage = res|> string
                     Console.WriteLine("Responding to Client {0} with result: {1}", getClientNumber clientID, responseMessage)
                     let responseBytes = Encoding.ASCII.GetBytes(responseMessage)
@@ -145,20 +176,25 @@ let handleClient (client : TcpClient) =
             with
             | :? System.IO.IOException -> continueCommunication <- false
 
+        // Close client connection after processing
         client.Close()
-        Console.WriteLine("Client disconnected: {0}", getClientNumber clientID)
     }
 
 
 let startServer () =
+
+    // Define server IP and port
     let serverIP = IPAddress.Parse("127.0.0.1")
     let serverPort = 12345
 
+    // Start listening to clients
     let listener = new TcpListener(serverIP, serverPort)
     listener.Start()
 
-    Console.WriteLine("Server listening on {0}:{1}", serverIP, serverPort)
+    Console.WriteLine("Server is running and listening on port {0}", serverPort)
 
+    // Recursive function to service threads concurrently by starting asynchronous
+    // task for every client that joins
     let rec acceptClients () =
         async {
             try
@@ -170,14 +206,11 @@ let startServer () =
                 return! acceptClients ()
             with
             | :? OperationCanceledException ->
-                // Handle server termination (e.g., log a message)
+                // Handle server termination
                 Console.WriteLine("Server terminated.")
                 listener.Stop()
         }
-
+    // Run acceptClients function asynchronously
     Async.RunSynchronously (acceptClients ())
-
-
-
 
 startServer()
